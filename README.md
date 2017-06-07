cb0402e890d106c18dd401921111089d600b8fe0
chris <chris@bumblehead.com>
2017-06-07 12:42:47 -0700

    do not block push state unless necessary

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 domhis
 ======
-**(c)[Bumblehead][0], 2014** [MIT-license](#license)
-
-### OVERVIEW:
+**(c)[Bumblehead][0]** [MIT-license](#license)
 
 `domhis` is a wrapper for the [`window.history`][1] method. Environments that provide `window.history` will use `window.history.pushState` to update a window location with a url, otherwise the url is defined on `window.location.href` (loading a new document).
 
@@ -23,32 +21,12 @@ When popstate occurs (back button press in a browser) the new url is passed to a
 [1]: https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history "history"
 
 ---------------------------------------------------------
-#### <a id="install"></a>Install:
-
-domhis may be downloaded directly or installed through `npm`.
-
- * **npm**
-
- ```bash
- $ npm install domhis
- ```
-
- * **Direct Download**
- 
- ```bash
- $ git clone https://github.com/iambumblehead/domhis.git
- ```
-
----------------------------------------------------------
-#### <a id="test"></a>Test:
+#### <a id="test"></a>test
 
 Tests are not automated and are performed by loading a document in the browser.
 
 - load `test/index.html` in your browser and run tests from the console.
 
-
----------------------------------------------------------
-#### <a id="license">License:
 
 ![scrounge](https://github.com/iambumblehead/scroungejs/raw/master/img/hand.png) 
 
--- domhis.js
+++ domhis.js
@@ -1,45 +1,42 @@
 // Filename: domhis.js
-// Timestamp: 2016.11.22-11:21:54 (last modified)
+// Timestamp: 2017.06.07-12:40:47 (last modified)
 // Author(s): Bumblehead (www.bumblehead.com)
 //
 // https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history
 // second param to pushState is ignored by firefox, the 'title' parameter
 //
 
-
-var domhis = module.exports = (function (win, loc, his) {
+let domhis = module.exports = ((win, loc, his, ispopping) => {
 
   win = typeof window === 'object' ? window : {};
   loc = win.location;
-  his = win.history;
+  his = win.history,
+  ispopping = false;
 
   return {
-    pushState : function (uri, obj) {
-      if (!this.f && typeof uri === 'string') {
+    pushState : (uri, obj) => {
+      if (!ispopping && typeof uri === 'string') {
         if (typeof his === 'object') {
-          his.pushState(obj || { uri : uri }, '', uri);      
+          his.pushState(obj || { uri : uri }, '', uri);
         } else {
           loc.href = loc.origin + uri.replace(/^([^\/])/, '/$1');
         }
       }
     },
 
-    onpopstate : function (fn) {
-      var that = this;
-
+    onpopstate : (fn) => {
       if (typeof fn === 'function' && his) {
-        win.onpopstate = function (e) {
-          that.f = true;
+        win.onpopstate = e => {
           if (e.state && e.state.uri) {
-            fn(e.state.uri, function () {
-              that.f = false;
-            });
+            ispopping = true;
+            fn(e.state.uri, () => ispopping = false);
           }
         };
       }
     }
   };
-}());
+  
+})();
 
 
 
